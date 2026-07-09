f1c47a88885767b37c675b005bf91c1e6cec34cc
Dishitha Gurram <57872584+dishithag@users.noreply.github.com>
2026-07-09 12:33:14 -0700

    Update config.go

--- config.go
+++ config.go
@@ -10,9 +10,7 @@ type Config struct {
 	S3AccessKey       string
 	S3SecretKey       string
 	NarrativeProvider string
-	GenAIHubEndpoint  string
 	GenAIHubModel     string
-	GenAIHubAPIKey    string
 }
 
 // LoadConfig builds a Config from environment variables with local-dev defaults.
@@ -24,9 +22,7 @@ func LoadConfig(serviceName string) *Config {
 		S3AccessKey:       envOr("S3_ACCESS_KEY", "minioadmin"),
 		S3SecretKey:       envOr("S3_SECRET_KEY", "minioadmin"),
 		NarrativeProvider: envOr("NARRATIVE_PROVIDER", ""),
-		GenAIHubEndpoint:  envOr("GENAI_HUB_ENDPOINT", ""),
 		GenAIHubModel:     envOr("GENAI_HUB_MODEL", ""),
-		GenAIHubAPIKey:    envOr("GENAI_HUB_API_KEY", ""),
 	}
 }
 
